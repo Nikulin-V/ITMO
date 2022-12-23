802982003615cd94c2653203abc52137438288e6
Vasiliy Nikulin <nikulin.vasily.777@ya.ru>
2022-12-23 17:42:30 +0300

    Enums Refection API Search

--- lab3/src/main/java/Main.java
+++ lab3/src/main/java/Main.java
@@ -1,73 +1,39 @@
 package main.java;
 
-import main.java.classes.Buffet;
-import main.java.classes.Building;
-import main.java.classes.Character;
-import main.java.classes.Tiger;
-import main.java.classes.builders.CharacterBuilder;
-import main.java.classes.builders.CharacterEngineer;
-import main.java.enums.Gender;
-import main.java.enums.Tone;
-import main.java.exceptions.CantBeHuggedException;
-import main.java.exceptions.EnvironmentVariableNotFoundException;
+import java.lang.reflect.Constructor;
 
 public class Main {
-    public static void main(String[] args) throws EnvironmentVariableNotFoundException {
+    public static void main(String[] args) throws NoSuchMethodException {
 
-        CharacterBuilder builder = new CharacterBuilder();
-        CharacterEngineer engineer = new CharacterEngineer(builder);
+        int parametersCount = 1;
+        String[] classNames = {
+                "Main", "Food", "Furniture", "Place", "CharacterBuilder", "CharacterEngineer",
+                "Bear", "Buffet", "Building", "Character", "Human", "Kangaroo", "Pig", "Tiger",
+                "Gender", "Tone", "CantBeHuggedException", "EnvironmentVariableNotFoundException", "ObjectNotFoundException"
+        };
+        String[] prefixes = {"absclasses", "classes", "enums", "exceptions"};
 
-        String bearNameEnv = "BEAR_NAME";
-        if (System.getenv(bearNameEnv) == null)
-            throw new EnvironmentVariableNotFoundException(bearNameEnv);
+        for (String className: classNames) {
+            Class<?> class_ = null;
+            for(String prefix: prefixes) {
 
-        Character pooh = engineer.manufactureCharacter(System.getenv(bearNameEnv), Gender.MALE);
-        Character piglet = engineer.manufactureCharacter("Пятачок", Gender.MALE);
-        Character christopherRobin = engineer.manufactureCharacter("Кристофер Робин", Gender.MALE);
-        Tiger tigger = new Tiger("Тигра", Gender.MALE);
-        Character kanga = engineer.manufactureCharacter("Кенга", Gender.FEMALE);
-        Character roo = engineer.manufactureCharacter("Крошка Ру", Gender.MALE);
-        
-        Building house = new Building("дом", kanga);
-        Buffet buffet = new Buffet("буфет");
+                try {
+                    class_ = Class.forName("main.java." + prefix + "." + className);
+                } catch (ClassNotFoundException ignored) {}
+                if (class_ != null) {
+                    break;
+                }
 
-        for (int i = 0; i < 3; i++) {
-            tigger.jump(Tone.CHEERFULLY);
-            tigger.say("Сюда идти?", christopherRobin);
+            }
+
+            if (class_ != null && class_.isEnum()) {
+                Constructor<?>[] constructors = class_.getDeclaredConstructors();
+                for (Constructor<?> constructor: constructors) {
+                    if (constructor.getParameterCount() == parametersCount + 2)
+                        System.out.println(class_.getName());
+                }
+            }
         }
 
-        house.addObject(christopherRobin);
-        tigger.move(house);
-
-        pooh.move(christopherRobin);
-        piglet.move(christopherRobin);
-        try {
-            pooh.hug(christopherRobin);
-            piglet.hug(christopherRobin);
-        } catch (CantBeHuggedException e) {
-            System.out.println(e.getMessage());
-        }
-        pooh.tell(christopherRobin, "в чём дело");
-
-        pooh.enter(house);
-        piglet.enter(house);
-        tigger.enter(house);
-        roo.sayHello(pooh);
-        roo.sayHello(piglet);
-        roo.sayHello(tigger);
-        roo.sayHello(tigger);
-        pooh.tell(kanga, "зачем они пришли");
-
-        kanga.say(
-                "Ну что ж, милый Тигра, загляни в мой буфет и посмотри -- что тебе там понравится",
-                tigger,
-                Tone.AFFECTIONATELY
-        );
-        kanga.understand("хотя с виду Тигра очень большой, он так же нуждается в ласке, как и Крошка Ру");
-
-        for (int potId = 0; potId < buffet.pots.length; potId++) {
-            tigger.stick(tigger.getBodyPart(), buffet.pots[potId]);
-        }
-        tigger.checkFoodFound();
     }
 }
