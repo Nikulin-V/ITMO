268abb4633b888f42bf65ddeae11ae4d81c21102
Vasiliy Nikulin <nikulin.vasily.777@ya.ru>
2022-12-28 14:45:27 +0300

    Enums Refection API Search via Reflections lib

--- lab3/src/main/java/Main.java
+++ lab3/src/main/java/Main.java
@@ -1,39 +1,53 @@
 package main.java;
 
+import org.reflections.Reflections;
+
 import java.lang.reflect.Constructor;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws NoSuchMethodException {
-
         int parametersCount = 1;
-        String[] classNames = {
-                "Main", "Food", "Furniture", "Place", "CharacterBuilder", "CharacterEngineer",
-                "Bear", "Buffet", "Building", "Character", "Human", "Kangaroo", "Pig", "Tiger",
-                "Gender", "Tone", "CantBeHuggedException", "EnvironmentVariableNotFoundException", "ObjectNotFoundException"
-        };
-        String[] prefixes = {"absclasses", "classes", "enums", "exceptions"};
 
-        for (String className: classNames) {
-            Class<?> class_ = null;
-            for(String prefix: prefixes) {
+        // Way 1 (via Reflections)
+        Reflections reflections = new Reflections("main.java");
+        Set<Class<? extends Enum>> enumClasses = reflections.getSubTypesOf(java.lang.Enum.class);
 
-                try {
-                    class_ = Class.forName("main.java." + prefix + "." + className);
-                } catch (ClassNotFoundException ignored) {}
-                if (class_ != null) {
-                    break;
-                }
-
-            }
-
-            if (class_ != null && class_.isEnum()) {
-                Constructor<?>[] constructors = class_.getDeclaredConstructors();
-                for (Constructor<?> constructor: constructors) {
-                    if (constructor.getParameterCount() == parametersCount + 2)
-                        System.out.println(class_.getName());
-                }
+        for (Class<?> class_: enumClasses) {
+            Constructor<?>[] constructors = class_.getDeclaredConstructors();
+            for (Constructor<?> constructor: constructors) {
+                if (constructor.getParameterCount() == parametersCount + 2)
+                    System.out.println(class_.getName());
             }
         }
+
+//        // Way 2
+//        String[] classNames = {
+//                "Main", "Food", "Furniture", "Place", "CharacterBuilder", "CharacterEngineer",
+//                "Bear", "Buffet", "Building", "Character", "Human", "Kangaroo", "Pig", "Tiger",
+//                "Gender", "Tone", "CantBeHuggedException", "EnvironmentVariableNotFoundException", "ObjectNotFoundException"
+//        };
+//        String[] prefixes = {"absclasses", "classes", "enums", "exceptions"};
+//
+//        for (String className: classNames) {
+//            Class<?> class_ = null;
+//            for(String prefix: prefixes) {
+//                try {
+//                    class_ = Class.forName("main.java." + prefix + "." + className);
+//                } catch (ClassNotFoundException ignored) {}
+//                if (class_ != null) {
+//                    break;
+//                }
+//            }
+//
+//            if (class_ != null && class_.isEnum()) {
+//                Constructor<?>[] constructors = class_.getDeclaredConstructors();
+//                for (Constructor<?> constructor: constructors) {
+//                    if (constructor.getParameterCount() == parametersCount + 2)
+//                        System.out.println(class_.getName());
+//                }
+//            }
+//        }
 
     }
 }
